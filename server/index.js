3fbd097d31d554014395aacd66c68b2ff1b92a94
Matthew Skomarovsky <matthew@verypolite.com>
2016-05-01 16:13:34 -0400

    basic server and web client send colors via websockets

---
+++ server/index.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+  socket.on("register-user", function(data) {
+    users.push(data);
+  });
+
+  socket.on("pick-color", function(data) {
+    console.log(data.from, "sent", data.color, "to", data.to);
+
+    io.emit("show-color", {
+      from: data.from,
+      to: data.to,
+      color: data.color
+    });
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
